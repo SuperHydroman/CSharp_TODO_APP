282a01ddcfb957204c65eee922175dcd7aed8b41
Gideon van den Herik <g.vandenherik@gmsnl.com>
2026-04-15 18:14:26 +0200

    Initial commit + Basic layout finished

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace TodoApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void NewTodoTextBox_OnKeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter) CreateTodo();
+    }
+
+    private void CreateTodo()
+    {
+        // TODO: Implement todo creation logic, preferable saving the data to a json file
+    }
+}
